7cc5f652149690071ba710794b3a21ae1b9e35ec
YBK <ybkamaleri@users.noreply.github.com>
2022-01-21 11:43:40 +0100

    PRE: Create filter for number of population

--- dev/befolkning.R
+++ dev/befolkning.R
@@ -1,8 +1,35 @@
-foldBF <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2021NESSTAR"
-fileBF <- "BEFOLK_GK_2021-01-26-12-08.csv"
+library(fs)
+foldBF <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2022NESSTAR"
+## fileBF <- "BEFOLK_GK_2021-01-26-12-08.csv"
 
-library(orgdata)
-DT <- fread(file.path(foldBF, fileBF), nrows = 10000)
+## Find the recent BEFOLKNING file, provided it's always called "BEFOLK_GK_*"
+dirFile <- fs::dir_ls(foldBF)
+fileBEF <- grep("BEFOLK_GK", dirFile, value = TRUE)
+yr <- gsub(".*(\\d{4})-(\\d{2})-(\\d{2})-(\\d{2})-(\\d{2}).csv$", "\\1\\2\\3\\4\\5", fileBEF)
+yrFile <- sort(as.numeric(yr), TRUE)
+dupFile <- any(yrFile[1], yrFile[-1])
+
+year <- yrFile[1]
+fileExt <- gsub("^(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})", "\\1_\\2-\\3-\\4-\\5", year)
+fileBEF <- paste0("BEFOLK_GK_", fileExt, ".csv")
+fileBEF
+
+library(data.table)
+DT <- fread(file.path(foldBF, fileBF))
+str(DT)
+dt <- DT[KJONN == 0 & ALDER == "0_120"]
+
+dt
+dt[, level := fcase(nchar(GEO) %in% 1:2, "F",
+                    nchar(GEO) %in% 3:4, "K",
+                    nchar(GEO) %in% 5:6, "B")]
+
+dt[GEO == 0 , level := "L"]
+dt[, stor := fifelse(TELLER >= 10000, 1, 0) ]
+dt[, .N, by = level]
+dt[, .N, by = stor]
+
+
 
 ## ## Koder som brukes i do fil for storkommunne
 ## ---------------------------------------------

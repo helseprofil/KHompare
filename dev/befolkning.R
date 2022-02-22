59a2210eb5c2c0fb3dcca1f950a40e19cb7e19e3
YBK <ybkamaleri@users.noreply.github.com>
2022-02-22 14:31:06 +0100

    Create file to select big and small kommune
    Befolkning file

--- dev/befolkning.R
+++ dev/befolkning.R
@@ -1,35 +1,48 @@
 library(fs)
-foldBF <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2022NESSTAR"
+source("dev/settings.R")
+foldBF <- file.path(sysDrive, "Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2022NESSTAR" )
 ## fileBF <- "BEFOLK_GK_2021-01-26-12-08.csv"
 
 ## Find the recent BEFOLKNING file, provided it's always called "BEFOLK_GK_*"
 dirFile <- fs::dir_ls(foldBF)
-fileBEF <- grep("BEFOLK_GK", dirFile, value = TRUE)
+fileBEF <- grep("BEFOLK_GK_\\d{4}", dirFile, value = TRUE)
 yr <- gsub(".*(\\d{4})-(\\d{2})-(\\d{2})-(\\d{2})-(\\d{2}).csv$", "\\1\\2\\3\\4\\5", fileBEF)
+yr
 yrFile <- sort(as.numeric(yr), TRUE)
-dupFile <- any(yrFile[1], yrFile[-1])
+## yrFile
+## dupFile <- any(yrFile[1], yrFile[-1])
+## dupFile
 
 year <- yrFile[1]
 fileExt <- gsub("^(\\d{4})(\\d{2})(\\d{2})(\\d{2})(\\d{2})", "\\1_\\2-\\3-\\4-\\5", year)
 fileBEF <- paste0("BEFOLK_GK_", fileExt, ".csv")
 fileBEF
 
+
 library(data.table)
-DT <- fread(file.path(foldBF, fileBF))
+DT <- fread(fileBEF)
 str(DT)
+
+# Select only total to find bigger and small kommuner
 dt <- DT[KJONN == 0 & ALDER == "0_120"]
 
-dt
+
 dt[, level := fcase(nchar(GEO) %in% 1:2, "F",
-                    nchar(GEO) %in% 3:4, "K",
+                    nchar(GEO) %in% 3:4, "k",
                     nchar(GEO) %in% 5:6, "B")]
 
 dt[GEO == 0 , level := "L"]
-dt[, stor := fifelse(TELLER >= 10000, 1, 0) ]
 dt[, .N, by = level]
-dt[, .N, by = stor]
+# Big and small kommuner with cutoff 10000
+dt[level == "k", level := fifelse(TELLER >= 10000, "K", "k") ]
 
+dt[, .N, by = level]
+dt
+saveRDS(dt, "dev/befolkning.rds")
 
+## dt[, .N, by = stor]
+## dt[, stor := fifelse(TELLER >= 10000, 1, 0)]
+## dt[level %in% c("K", "k"), .N, by = stor]
 
 ## ## Koder som brukes i do fil for storkommunne
 ## ---------------------------------------------

b249f96658b64fdc1cc7ebc6894368745bafb95f
Yusman Kamaleri <ybkamaleri@gmail.com>
2022-02-25 00:11:35 +0100

    Test: Drive windows

--- tests/testthat/test-directory.R
+++ tests/testthat/test-directory.R
@@ -1,11 +1,11 @@
 
 test_that("Get directory", {
   dirLinux <- "/mnt/F/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2022NESSTAR"
-  dirLinux2 <- "/mnt/F/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2021NESSTAR"
+  dirWin <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/PRODUKTER/KUBER/KOMMUNEHELSA/KH2021NESSTAR"
 
   withr::with_options(list(kh.year = 2022),
                       expect_equal(get_dir("current", "Linux"), dirLinux))
 
   withr::with_options(list(kh.year = 2022),
-                      expect_equal(get_dir("previous", "Linux"), dirLinux2))
+                      expect_equal(get_dir("previous", "Windows"), dirWin))
 })

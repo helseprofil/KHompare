9758e68a50d7c105441c068bca79e4847a379367
YBK <ybkamaleri@users.noreply.github.com>
2022-03-04 17:10:16 +0100

    Feat: View outliers

---
+++ tests/testthat/test-view.R
@@ -0,0 +1,15 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("View outliers", {
+
+  dt <- readRDS(file.path(system.file("test-data", "view-DT-file.rds", package = "KHompare")))
+  dtout <- readRDS(file.path(system.file("test-data", "view-dt.rds", package = "KHompare")))
+  dd <- dtout[1:2]
+
+  expect_equal(row_num(4), 1:4)
+  expect_equal(view_outliers(dt, "MEIS"), dtout)
+  expect_equal(view_outliers(dt, "MEIS", 2), dd)
+  expect_error(view_outliers(dt, "NOTHING"))
+})
